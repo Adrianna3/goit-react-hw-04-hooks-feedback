e749817ab77d5c4d76c7c37f1ce9bc6c8d8f9f79
Adrianna Staniek <adrstani@wp.pl>
2022-12-22 23:53:54 +0100

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,71 @@
-import React, { Component } from 'react';
+import React, { useState} from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOption } from './FeedbackOption/FeedbackOption';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const total = this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    const total = good + neutral + bad;
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const total = this.state.good + this.state.neutral + this.state.bad;
-    const positive = Math.round((this.state.good / total) * 100);
+  const countPositiveFeedbackPercentage = () => {
+    const total = good + neutral + bad;
+    const positive = Math.round((good / total) * 100);
     // console.log(positivePercentage)
     return isNaN(positive) ? 0 : positive;
   };
 
-  updateGood = () => {
- this.setState(oldState => ({ ...oldState, good: oldState.good + 1 }));
+  const updateGood = () => {
+    setGood(good + 1);
   };
 
-  updateNeutral = () => {
-    this.setState(oldState => ({ ...oldState, neutral: oldState.neutral + 1 }));  };
+  const updateNeutral = () => {
+    setNeutral(neutral + 1);
+  };
 
-  updateBad = () => {
-this.setState(oldState => ({ ...oldState, bad: oldState.bad + 1 }));  };
+  const updateBad = () => {
+    setBad(bad + 1);
+  };
 
-  // updateFields = key => {
-  //   console.log('idzie');
-  //   this.setState({ [key]: this.state[key] + 1 });
-  // };
+  return (
+    <div>
+      <Section tittle="Please leave feedback">
+        <div style={{ display: 'flex' }}>
+          <FeedbackOption options={'good'} onLeaveFeedback={updateGood} />
+          <FeedbackOption options={'neutral'} onLeaveFeedback={updateNeutral} />
+          <FeedbackOption options={'bad'} onLeaveFeedback={updateBad} />
+        </div>
+      </Section>
 
-  render() {
-    return (
-      <div>
-        <Section tittle="Please leave feedback">
-          <div style={{ display: 'flex' }}>
-            <FeedbackOption
-              options={'good'}
-              onLeaveFeedback={this.updateGood}
+      <Section tittle="Statistics">
+        <div>
+          {countTotalFeedback() !== 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positive={countPositiveFeedbackPercentage()}
             />
-            <FeedbackOption
-              options={'neutral'}
-              onLeaveFeedback={this.updateNeutral}
-            />
-            <FeedbackOption options={'bad'} onLeaveFeedback={this.updateBad} />
-          </div>
-        </Section>
-
-        <Section tittle="Statistics">
-          <div>
-            {this.countTotalFeedback() !== 0 ? (
-              <Statistics
-                good={this.state.good}
-                neutral={this.state.neutral}
-                bad={this.state.bad}
-                total={this.countTotalFeedback()}
-                positive={this.countPositiveFeedbackPercentage()}
-              />
-            ) : (
-              <Notification message="There is no feedback"></Notification>
-            )}
-          </div>
-        </Section>
-      </div>
-    );
-  }
-}
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </div>
+      </Section>
+    </div>
+  );
+};
 
 export default App;
